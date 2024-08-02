41dc5cca5ab27fc017cebe502360ec6e3e0b33bc
Eduardo Watanabe <eduardowatanabelima@gmail.com>
2024-08-02 13:15:50 -0300

    arrumando exception de usuário não encontrado

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Architecture\Services\User\UserService;
 use App\Http\Resources\UserResource;
+use ErrorException;
 use Exception;
 
 class UsersController extends Controller {
@@ -17,14 +18,26 @@ class UsersController extends Controller {
     public function getById(int $id) {
         try {
             $user = $this->userService->getById($id);
+
+            if ($user == null) {
+                throw new ErrorException("Usuário não encontrado");
+            }
+
             $user = new UserResource($user);
 
             return response()->json($user);
-        } catch(Exception $e) {
+        }
+        catch (ErrorException $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ], 404);
+        }
+        catch(Exception $e) {
             return response()->json([
                 'error' => 'An error occurred while processing your request.'
             ], 500);
         }
+
     }
 
 }
